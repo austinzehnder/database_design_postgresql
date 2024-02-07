7cee602a2fba4522fecce056e28106871c8e0ce8
Austin L. Zehnder <134451398+austinzehnder@users.noreply.github.com>
2024-02-07 15:07:53 -0800

    Create stored_procedure.sql

---
+++ stored_procedure.sql
@@ -0,0 +1,25 @@
+-- INIT
+DROP TABLE IF EXISTS keyvalue;
+
+CREATE TABLE keyvalue ( 
+  id SERIAL,
+  key VARCHAR(128) UNIQUE,
+  value VARCHAR(128) UNIQUE,
+  created_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+  updated_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+  PRIMARY KEY(id)
+);
+
+-- Create Stored Procedure
+CREATE OR REPLACE FUNCTION update_updated_at()
+RETURNS TRIGGER AS $$
+BEGIN
+  NEW.updated_at = NOW();
+  RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER update_updated_at_trigger
+BEFORE UPDATE ON keyvalue
+FOR EACH ROW
+EXECUTE PROCEDURE update_updated_at();
